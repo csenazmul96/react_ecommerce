d0393e2fcfdb6b3e1d27d77e9960e48a733e04ec
Nazmul <csenazmul96@gmail.com>
2022-05-31 21:12:20 +0600

    cart table show

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,144 +1,138 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import {GetCart} from "../Services/Actions/CartAction";
-import {connect} from "react-redux";
+import {connect, useSelector} from "react-redux";
 
 const Cart = ({cartItems, GetCart})=>{
+    const [items, setItems]= useState([])
+    let [totalPack, settotalPack] = useState(0)
+    let [totalQty, settotalQty] = useState(0)
+    let [totalPrice, settotalPrice] = useState(0.00)
+    useEffect(() => {
+        setItems(cartItems)
+        CalculateSummary()
+    }, [cartItems]);
+
+    const changeQty = (i, type)=>{
+        const list = [...items]
+        if(type === 'plus') {
+            list[i].quantity = list[i].quantity + 1
+        } else if(type === 'minus' && list[i].quantity > 0) {
+            list[i].quantity = list[i].quantity - 1
+        }
+        setItems(list)
+        CalculateSummary()
+    }
+    const handleInputChange = (e,i)=>{
+        const name = e.target.name
+        const value = e.target.value
+        const list = [...items]
+        list[i][name] = value
+        setItems(list)
+        CalculateSummary()
+    }
+
+    function CalculateSummary(){
+        if(items && items.length){
+            let sum = 0;
+            items.forEach((data, i) => {
+                let packSize = data.item && data.item.pack ? data.item.pack.total_item : 1;
+                let total = data.quantity * data.item.price * packSize
+                sum = sum + total
+            })
+            settotalPrice(sum ? sum.toFixed(2) : 0.00)
+        }
+    }
+
+    const CartItemDesktop = () =>{
+        if(items && items.length) {
+            return (
+                items.map((inv, i) => {
+                    return (<tr key={'desktop_cart'+i}>
+                        <td>
+                            <div className="cart_product_inner">
+                                <a href="#">{ inv.item && inv.item.images && inv.item.images.length ? (<img src={inv.item.images[0].thumbs_image} alt="" className="img-fluid" width="70px" />) : null}</a>
+                                <div className="cart_product_text">
+                                    {inv.item ? <h2>{inv.item.style_no}</h2> : null}
+                                    {inv.item ? <p>{inv.item.name}</p> : null}
+                                    {inv.item ? <h2>${inv.item.price.toFixed(2)}</h2> : null}
+                                </div>
+                                <span className="remove">Remove</span>
+                            </div>
+                        </td>
+                        <td>
+                            <div className="num_count_wrapper">
+                                <span>Quantity: </span>
+                                <div className="num_count">
+                                    <div id="minus" className="minus" onClick={e=>changeQty(i, 'minus')}>
+                                        <button className="btn btn-default"><i className="lni lni-minus"></i></button>
+                                    </div>
+                                    <input id="qty1" type="text" value={inv.quantity} className="qty" onChange={e=>handleInputChange(e, i)} />
+                                    <div id="add" className="add" onClick={e=>changeQty(i, 'plus')}>
+                                        <button className="btn btn-default"><i className="lni lni-plus"></i></button>
+                                    </div>
+                                </div>
+                            </div>
+                        </td>
+                    </tr>)
+                })
+            )
+        }
+    }
+    const CartItemMobile = () =>{
+        if(items && items.length) {
+            return (
+                items.map((inv, i) => {
+                    return (<div className="cart_main_mobile_inner" key={'cart_mob'+i}>
+                        <span className="remove"><div className="close_icon"></div></span>
+                        <div className="left">
+                            <div className="cart_product_text">
+                                {inv.item ? <h2>{inv.item.style_no}</h2> : null}
+                                {inv.item ? <p>{inv.item.name}</p> : null}
+                                {inv.item ? <h2>${inv.item.price.toFixed(2)}</h2> : null}
+                            </div>
+                            <div className="qnty">Quantity:</div>
+                            <div className="num_count_wrapper">
+                                <div className="num_count">
+                                    <div id="minus" className="minus" onClick={e=>changeQty(i, 'minus')}>
+                                        <button className="btn btn-default"><i className="lni lni-minus"></i></button>
+                                    </div>
+                                    <input id="qty1" type="text" value={inv.quantity} className="qty" onChange={e=>handleInputChange(e, i)} />
+                                    <div id="add" className="add" onClick={e=>changeQty(i, 'plus')}>
+                                        <button className="btn btn-default"><i className="lni lni-plus"></i></button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="right">
+                            <a href="#">{ inv.item && inv.item.images && inv.item.images.length ? (<img src={inv.item.images[0].thumbs_image} alt="" className="img-fluid" />) : null}</a>
+                        </div>
+                    </div>)
+                })
+            )
+        }
+    }
     return(
         <section className="cart_area">
             <div className="container custom_container">
                 <div className="row">
                     <div className="col-md-12">
                         <div className="cart_title">
-                            <h2>Shipping Cart (2)</h2>
+                            <h2>Shipping Cart ({items && items.length ? items.length : 0})</h2>
                         </div>
                     </div>
                     <div className="col-md-9">
                         <div className="cart_main_content m_none">
-                            <p>Item</p>
                             <div className="table-responsive">
                                 <table className="table">
-                                    <tr>
-                                        <td>
-                                            <div className="cart_product_inner">
-                                                <a href="#"><img src="../images/productnew/product-14.webp"
-                                                                 alt="" className="img-fluid" /></a>
-                                                <div className="cart_product_text">
-                                                    <h2>HPP9687</h2>
-                                                    <p>Thunder Blue</p>
-                                                    <h2>$82.50</h2>
-                                                </div>
-                                                <span className="remove">Remove</span>
-                                            </div>
-                                        </td>
-                                        <td>
-                                            <div className="num_count_wrapper">
-                                                <span>Quantity: </span>
-                                                <div className="num_count">
-                                                    <div id="minus" className="minus">
-                                                        <button className="btn btn-default"><i
-                                                            className="lni lni-minus"></i></button>
-                                                    </div>
-                                                    <input id="qty1" type="text" value="1" className="qty" />
-                                                        <div id="add" className="add">
-                                                            <button className="btn btn-default"><i
-                                                                className="lni lni-plus"></i></button>
-                                                        </div>
-                                                </div>
-
-
-                                            </div>
-                                        </td>
-                                    </tr>
-
+                                    <tbody>
+                                    <CartItemDesktop />
+                                    </tbody>
                                 </table>
                             </div>
                         </div>
                         <div className="d_none">
                             <div className="cart_main_content_mobile">
-                                <div className="cart_main_mobile_inner">
-                                    <span className="remove"><div className="close_icon"></div></span>
-                                    <div className="left">
-                                        <div className="cart_product_text">
-                                            <h2>HPP9687</h2>
-                                            <p>Thunder Blue</p>
-                                            <h2>$82.50</h2>
-                                        </div>
-                                        <div className="qnty">Quantity:</div>
-                                        <div className="num_count_wrapper">
-                                            <div className="num_count">
-                                                <div id="minus" className="minus">
-                                                    <button className="btn btn-default"><i className="lni lni-minus"></i></button>
-                                                </div>
-                                                <input id="qty1" type="text" value="1" className="qty" />
-                                                    <div id="add" className="add">
-                                                        <button className="btn btn-default"><i className="lni lni-plus"></i></button>
-                                                    </div>
-                                            </div>
-
-
-                                        </div>
-                                    </div>
-                                    <div className="right">
-                                        <a href="#"><img src="../images/productnew/product-14.webp" alt="" className="img-fluid" /></a>
-                                    </div>
-                                </div>
-                                <div className="cart_main_mobile_inner">
-                                    <span className="remove"><div className="close_icon"></div></span>
-                                    <div className="left">
-                                        <div className="cart_product_text">
-                                            <h2>HPP9687</h2>
-                                            <p>Thunder Blue</p>
-                                            <h2>$82.50</h2>
-                                        </div>
-                                        <div className="qnty">Quantity:</div>
-                                        <div className="num_count_wrapper">
-                                            <div className="num_count">
-                                                <div id="minus" className="minus">
-                                                    <button className="btn btn-default"><i className="lni lni-minus"></i></button>
-                                                </div>
-                                                <input id="qty1" type="text" value="1" className="qty" />
-                                                    <div id="add" className="add">
-                                                        <button className="btn btn-default"><i className="lni lni-plus"></i></button>
-                                                    </div>
-                                            </div>
-
-
-                                        </div>
-                                    </div>
-                                    <div className="right">
-                                        <a href="#"><img src="../images/productnew/product-14.webp" alt="" className="img-fluid" /></a>
-                                    </div>
-                                </div>
-                                <div className="cart_main_mobile_inner">
-                                    <span className="remove"><div className="close_icon"></div></span>
-                                    <div className="left">
-                                        <div className="cart_product_text">
-                                            <h2>HPP9687</h2>
-                                            <p>Thunder Blue</p>
-                                            <h2>$82.50</h2>
-                                        </div>
-                                        <div className="qnty">Quantity:</div>
-                                        <div className="num_count_wrapper">
-                                            <div className="num_count">
-                                                <div id="minus" className="minus">
-                                                    <button className="btn btn-default"><i
-                                                        className="lni lni-minus"></i></button>
-                                                </div>
-                                                <input id="qty1" type="text" value="1" className="qty" />
-                                                    <div id="add" className="add">
-                                                        <button className="btn btn-default"><i
-                                                            className="lni lni-plus"></i></button>
-                                                    </div>
-                                            </div>
-
-
-                                        </div>
-                                    </div>
-                                    <div className="right">
-                                        <a href="#"><img src="../images/productnew/product-14.webp" alt=""
-                                                         className="img-fluid" /></a>
-                                    </div>
-                                </div>
+                                <CartItemMobile />
                             </div>
                         </div>
                     </div>
@@ -146,6 +140,7 @@ const Cart = ({cartItems, GetCart})=>{
                         <div className="cart_right">
                             <h2>Order Summery</h2>
                             <table className="table">
+                                <tbody>
                                 <tr>
                                     <td>Taxes</td>
                                     <td>Calculated In Checkout</td>
@@ -156,8 +151,9 @@ const Cart = ({cartItems, GetCart})=>{
                                 </tr>
                                 <tr>
                                     <td>Estimated Total</td>
-                                    <td>$145.56</td>
+                                    <td>${totalPrice}</td>
                                 </tr>
+                                </tbody>
                             </table>
                             <div className="btn_common_wrap">
                                 <button className="btn_common">Proced To Checkout</button>
